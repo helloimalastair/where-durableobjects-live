15963092e6af96d49481c02a1452c907a927329b
helloimalastair <37487746+helloimalastair@users.noreply.github.com>
2024-01-23 13:38:20 +0100

    halve latency data

--- src/DO.ts
+++ src/DO.ts
@@ -1,9 +1,9 @@
 export class DO {
 	async fetch() {
 		return new Response(
-			((
+			`["${((
 				await (await fetch("https://www.cloudflare.com/cdn-cgi/trace")).text()
-			).match(/^colo=(.+)/m) as string[])[1],
+			).match(/^colo=(.+)/m) as string[])[1]}", ${Date.now()}]`
 		);
 	}
 }
--- src/genData.ts
+++ src/genData.ts
@@ -6,24 +6,21 @@ const JURISDICTIONS = ["eu", "fedramp"] as const;
 export default async function (c: Context<{ Bindings: Environment }>) {
 	const originColo = (c.req.raw.cf as IncomingRequestCfProperties).colo;
 	const start = Date.now();
-	const durableColo = await (await c.env.DO.get(c.env.DO.newUniqueId()).fetch(c.req.raw)).text();
-	const end = Date.now();
+	const [durableColo, end] = await (await c.env.DO.get(c.env.DO.newUniqueId()).fetch(c.req.raw)).json<[string, number]>();
 	c.executionCtx.waitUntil((async (): Promise<void> => {
 		c.env.WDL.writeDataPoint({ indexes: [durableColo], blobs: [originColo], doubles: [(end - start) / 2] });
 		await fetch("https://1.1.1.1/cdn-cgi/trace"); // Reset timer
 		for (const region of REGIONS) {
 			const start = Date.now();
-			const durableColo = await (await c.env.DO.get(c.env.DO.newUniqueId(), {
+			const [durableColo, end] = await (await c.env.DO.get(c.env.DO.newUniqueId(), {
 				locationHint: region,
-			}).fetch(c.req.raw)).text();
-			const end = Date.now();
+			}).fetch(c.req.raw)).json<[string, number]>();
 			c.env.WDL.writeDataPoint({ indexes: [durableColo], blobs: [originColo, region], doubles: [(end - start) / 2] });
 		}
 		for(const jurisdiction of JURISDICTIONS) {
 			const subnamespace = c.env.DO.jurisdiction(jurisdiction);
 			const start = Date.now();
-			const durableColo = await (await subnamespace.get(subnamespace.newUniqueId()).fetch(c.req.raw)).text();
-			const end = Date.now();
+			const [durableColo, end] = await (await subnamespace.get(subnamespace.newUniqueId()).fetch(c.req.raw)).json<[string, number]>();
 			c.env.WDL.writeDataPoint({ indexes: [durableColo], blobs: [originColo, "", jurisdiction], doubles: [(end - start) / 2] });
 		}
 	})());
