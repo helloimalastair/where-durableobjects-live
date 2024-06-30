a43077e265561b8d6105b7a9d7384868a8fb2e30
helloimalastair <37487746+helloimalastair@users.noreply.github.com>
2024-06-30 18:33:19 +0100

    bump iata data and remove some parentheses

--- packages/iata/build/regions.ts
+++ packages/iata/build/regions.ts
@@ -5,12 +5,17 @@ interface Parsed {
 	name: string,
 }
 
+const filter = /\((city|town|capital city|municipality|federal territory|autonomous district|community|capital district|autonomous city|national capital territory|special city|autonomous territorial unit|administrative territorial units|provincial municipality|district|state|area outside territorial authority|oil platforms|special administrative area|autonomous republic)\)/gi;
+
 export default async function getRegions() {
 	const raw = await Bun.file("data/regions.csv").text();
 	return ((parse(raw, { header: true, skipEmptyLines: "greedy" })).data as unknown as Parsed[])
-		.reduce((acc, { code, name }) => {
-			if(!code || !name || name === "(unassigned)") return acc;
-			acc[code] = name;
-			return acc;
-		}, {} as Record<string, string>);
+	.reduce((acc, { code, name }) => {
+		if(!code ||
+			!name ||
+			name.startsWith("(unassigned)")
+		) return acc;
+		acc[code] = name.replaceAll(filter, "").trim();
+		return acc;
+	}, {} as Record<string, string>);
 }
